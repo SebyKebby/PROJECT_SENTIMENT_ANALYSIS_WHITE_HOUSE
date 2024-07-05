2617cd4d7a335f7dba1e243a88537c69923ca7a8
SebbyKebby <109518181+SebyKebby@users.noreply.github.com>
2024-07-05 10:29:53 +0700

    fixed step 3 : fixed the output returning "\n" and cleaned the data. Deleted outdated output files

--- STEP_3_APPROVAL_RATING_SCRAPING/approval_rating/approval_rating/pipelines.py
+++ STEP_3_APPROVAL_RATING_SCRAPING/approval_rating/approval_rating/pipelines.py
@@ -7,7 +7,21 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
-
 class ApprovalRatingPipeline:
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        ##CLEANING ALL DATAS
+        keys = ['start_date', 'end_date' , 'approving' , 'disapproving', 'unsure']
+        for key in keys:
+            key_value = adapter.get(key)
+            key_value = key_value.strip()
+
+        ## CLEANING THE DATE DATA
+        date_keys = ['start_date', 'end_date']
+        for date_key in date_keys:
+            date_value = adapter.get(date_key)
+            date_value = date_value.replace('-','/')
         return item
+
+
--- STEP_3_APPROVAL_RATING_SCRAPING/approval_rating/approval_rating/spiders/approvalRatingSpider.py
+++ STEP_3_APPROVAL_RATING_SCRAPING/approval_rating/approval_rating/spiders/approvalRatingSpider.py
@@ -10,10 +10,10 @@ class ApprovalratingspiderSpider(scrapy.Spider):
         table_rows = response.xpath("//div[@class='field-body']/table/tbody/tr")
         table_data = ApprovalItems()
         for table_row in table_rows:
-            table_data['start_date'] = table_row.xpath("td[1]/text()").get()
-            table_data['end_date']  = table_row.xpath("td[2]/text()").get()
-            table_data['approving'] = table_row.xpath("td[3]/text()").get()
-            table_data['disapproving'] = table_row.xpath("td[4]/text()").get()
-            table_data['unsure'] = table_row.xpath("td[5]/text()").get()
+            table_data['start_date'] = table_row.xpath("td[1]//text()").get() #Added // because the original code did not work since one element was nested in a "p" element (while others arent). 
+            table_data['end_date']  = table_row.xpath("td[2]//text()").get()
+            table_data['approving'] = table_row.xpath("td[3]//text()").get()
+            table_data['disapproving'] = table_row.xpath("td[4]//text()").get()
+            table_data['unsure'] = table_row.xpath("td[5]//text()").get()
             yield table_data
-        pass
+        
